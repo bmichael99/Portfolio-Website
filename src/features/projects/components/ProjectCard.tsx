5893e39ac76b4280795026d1e3f99d43add4d98c
Michael <michaelbrandmeyer99@gmail.com>
2026-05-05 17:03:40 -0700

    feat: project card to display project information, text, and images. WIP

---
+++ src/features/projects/components/ProjectCard.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { type Project } from '../ProjectPage'
+import { ExternalLink, GitBranch } from 'lucide-react'
+
+function ProjectCard({title, summary, description, github_link, image_url, demo_link}: Project) {
+  return (
+    <div className='py-4 w-full hover:outline-1 rounded-sm p-2 flex flex-col gap-2'>
+      <h2 className='font-medium'>{title}</h2>
+      <p className='text-muted-foreground'>{description}</p>
+      {/*Links*/}
+      <div className='flex gap-2 w-full'>
+        <a className='outline-1 py-1 px-2 rounded-lg text-sm hover:bg-accent flex items-center gap-1' href={github_link} target='_blank'><GitBranch height={"14px"} width={"14px"}/> Github</a>
+        {demo_link && <a className='outline-1 py-1 px-2 rounded-lg text-sm hover:bg-accent flex items-center gap-1' href={demo_link} target='_blank'><ExternalLink height={"14px"} width={"14px"}/> Live Demo</a>}
+      </div>
+    </div>
+  )
+}
+
+export default ProjectCard
