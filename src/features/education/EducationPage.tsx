a468bef824fd9dbd66aefd3510f28e17c711cfed
Michael <michaelbrandmeyer99@gmail.com>
2026-05-06 22:47:00 -0700

    init

---
+++ src/features/education/EducationPage.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+type Props = {}
+
+function EducationPage({}: Props) {
+  return (
+    <div className='w-full'>
+        <h2 className='font-bold text-3xl py-4'>Education</h2>
+        <hr></hr>
+    </div>
+  )
+}
+
+export default EducationPage
---
+++ src/features/experience/ExperiencePage.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+type Props = {}
+
+function ExperiencePage({}: Props) {
+  return (
+    <div className='w-full'>
+        <h2 className='font-bold text-3xl py-4'>Experience</h2>
+        <hr></hr>
+    </div>
+  )
+}
+
+export default ExperiencePage
