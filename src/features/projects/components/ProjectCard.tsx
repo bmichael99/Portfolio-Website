20c1c022b1905b205494631ba628beaed48d9626
Michael <michaelbrandmeyer99@gmail.com>
2026-05-06 22:46:39 -0700

    feat: include tech badges in project card

--- src/features/projects/components/ProjectCard.tsx
+++ src/features/projects/components/ProjectCard.tsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import { type Project } from '../ProjectPage'
+import { techList, techLabelsColored, type TechList } from '../../../lib/tech'
 import { ExternalLink, GitBranch } from 'lucide-react'
+import TechBadge from '../../technology/components/TechBadge'
+ 
 
-function ProjectCard({title, summary, description, github_link, image_url, demo_link}: Project) {
+
+function ProjectCard({title, summary, description, github_link, image_url, demo_link, technology_used }: Project) {
   return (
     <div className='py-4 w-full hover:outline-1 rounded-sm p-2 flex flex-col gap-2'>
       <h2 className='font-medium'>{title}</h2>
       <p className='text-muted-foreground'>{description}</p>
+      {/*Technology Used*/}
+      <div className='flex gap-2 w-full pb-4 flex-wrap'>
+        {technology_used?.map((tech) => <TechBadge icon={techLabelsColored[tech].icon} label={techLabelsColored[tech].label}></TechBadge>)}
+      </div>
       {/*Links*/}
       <div className='flex gap-2 w-full'>
         <a className='outline-1 py-1 px-2 rounded-lg text-sm hover:bg-accent flex items-center gap-1' href={github_link} target='_blank'><GitBranch height={"14px"} width={"14px"}/> Github</a>
