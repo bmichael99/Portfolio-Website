aa99dfdbacebc6136f8ca87fb9b8a19067b80b0d
Michael <michaelbrandmeyer99@gmail.com>
2026-05-09 22:04:08 -0700

    WIP: started adding image, but we need to refactor this component to be of fixed size using grid and have styling rules for overflowing content.

--- src/features/projects/components/ProjectCard.tsx
+++ src/features/projects/components/ProjectCard.tsx
@@ -8,18 +8,25 @@ import TechBadge from '../../technology/components/TechBadge'
 
 function ProjectCard({title, summary, description, github_link, image_url, demo_link, technology_used }: Project) {
   return (
-    <div className='py-4 w-full hover:outline-1 rounded-sm p-2 flex flex-col gap-2'>
-      <h2 className='font-medium'>{title}</h2>
-      <p className='text-muted-foreground'>{description}</p>
-      {/*Technology Used*/}
-      <div className='flex gap-2 w-full pb-4 flex-wrap'>
-        {technology_used?.map((tech) => <TechBadge icon={techLabelsColored[tech].icon} label={techLabelsColored[tech].label}></TechBadge>)}
+    
+    <div className='grid gap-4 items-center md:grid-cols-[auto_1fr] p-2 hover:outline-1 rounded-3xl'>
+      {/*Project Photo*/}
+      <img className='outline-1 w-[350px] h-[250px] rounded-3xl justify-self-center'></img>
+      {/*Project Info*/}
+      <div className='py-4 w-full  rounded-sm flex flex-col gap-2'>
+        <h2 className='font-medium'>{title}</h2>
+        <p className='text-muted-foreground'>{summary}</p>
+        {/*Technology Used*/}
+        <div className='flex gap-2 w-full pb-4 flex-wrap'>
+          {technology_used?.map((tech) => <TechBadge icon={techLabelsColored[tech].icon} label={techLabelsColored[tech].label}></TechBadge>)}
+        </div>
+        {/*Links*/}
+        <div className='flex gap-2 w-full'>
+          <a className='outline-1 py-1 px-2 rounded-lg text-sm hover:bg-accent flex items-center gap-1' href={github_link} target='_blank'><GitBranch height={"14px"} width={"14px"}/> Github</a>
+          {demo_link && <a className='outline-1 py-1 px-2 rounded-lg text-sm hover:bg-accent flex items-center gap-1' href={demo_link} target='_blank'><ExternalLink height={"14px"} width={"14px"}/> Live Demo</a>}
+        </div>
       </div>
-      {/*Links*/}
-      <div className='flex gap-2 w-full'>
-        <a className='outline-1 py-1 px-2 rounded-lg text-sm hover:bg-accent flex items-center gap-1' href={github_link} target='_blank'><GitBranch height={"14px"} width={"14px"}/> Github</a>
-        {demo_link && <a className='outline-1 py-1 px-2 rounded-lg text-sm hover:bg-accent flex items-center gap-1' href={demo_link} target='_blank'><ExternalLink height={"14px"} width={"14px"}/> Live Demo</a>}
-      </div>
+      
     </div>
   )
 }
