5e36bd6a5fe5e621579448c5707386fb090efb2f
Michael <michaelbrandmeyer99@gmail.com>
2026-05-09 22:03:28 -0700

    feat: infinite caraousel displaying tech stack.

--- src/features/technology/TechPage.tsx
+++ src/features/technology/TechPage.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { techList, techLabelsColored, } from '../../lib/tech'
 import TechBadge from './components/TechBadge'
+import TechCarousel from './components/TechCarousel'
 
 type Props = {}
 
@@ -8,11 +9,13 @@ type Props = {}
 function TechPage({}: Props) {
   return (
     <div className='w-full'>
-        <h2 className='font-bold text-3xl py-4'>Technology</h2>
-        <hr></hr>
-        <div className='flex gap-4 flex-wrap py-4'>
-        {techList.map((tech) => <TechBadge icon={techLabelsColored[tech].icon} label={techLabelsColored[tech].label}/>)}
-        </div>
+      <h2 className='font-bold text-3xl py-4'>My Tech Stack</h2>
+      <hr></hr>
+      {/*Change Tech Stack back to the format below if the carousel is too inconventient*/}
+      {/* <div className='flex gap-4 flex-wrap py-4'>
+      {techList.map((tech) => <TechBadge icon={techLabelsColored[tech].icon} label={techLabelsColored[tech].label}/>)}
+      </div> */}
+      <TechCarousel></TechCarousel>
     </div>
   )
 }
